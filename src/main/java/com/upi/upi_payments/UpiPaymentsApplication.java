e7efd4f9e562fa222255c74a591860e0fa5f19be
Rakesh8086 <rakeshteja7007@gmail.com>
2025-07-27 18:13:01 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/upi/upi_payments/UpiPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.upi.upi_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upi/upi_payments/UpiPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upi.upi_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
