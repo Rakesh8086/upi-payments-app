1cee36a319a5d761b3aa8a705d6d193e9c556ef9
Rakesh8086 <rakeshteja7007@gmail.com>
2025-07-29 20:22:00 +0530

    Added userDTO and userController classes

--- src/main/java/com/upi/upi_payments/controller/UserController.java
+++ src/main/java/com/upi/upi_payments/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.upi.upi_payments.controller;
 
+import com.upi.upi_payments.dto.UserRegistrationDTO;
+import com.upi.upi_payments.entity.User;
+import com.upi.upi_payments.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// controller is like a front desk. Handles requests, sends respones to service layer
+// notice how every layer is separated - "Separation of concerns" - easy to test, debug
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody UserRegistrationDTO request){
+        return userService.registerUser(
+            request.getUserName(),
+            request.getPhoneNumber(),
+            request.getUserType()
+        );
+    }
 }
--- src/main/java/com/upi/upi_payments/dto/UserRegistrationDTO.java
+++ src/main/java/com/upi/upi_payments/dto/UserRegistrationDTO.java
@@ -1,5 +1,34 @@
 package com.upi.upi_payments.dto;
 
-public class UserRegistrationDTO {
-    
+import com.upi.upi_payments.entity.UserType;
+
+// data transfor object class is exposed in the frontend for getting user input
+// do not expose the entity fields like id to the frontend user
+// so use a separate class to get input from frontend
+
+public class UserRegistrationDTO{
+    private String userName;
+    private String phoneNumber;
+    private UserType userType;
+
+    public String getUserName(){
+        return userName;
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserType getUserType(){
+        return userType;
+    }
+    public void setUserType(UserType userType){
+        this.userType = userType;
+    }
 }
